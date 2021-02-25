c0a5f751da780a6f1b2085c93eb56d0587e4b71e
Mariana Rodrigues <rodrigues.mariannarf@gmail.com>
2021-02-25 14:09:23 -0300

    exemplo com funções

--- exemplo14.py
+++ exemplo14.py
@@ -9,14 +9,22 @@ print(lista)
 numero = [cont **2 for cont in range(20)]
 print(numero)
 
-#exemplo 4: funções anônimas\ lambda
+#exemplo 4: funções anônimas\lambda
 
 soma = lambda x, y: x+y
 soma(4,5)
 print(soma(4,5))
 
-#exemplo 4.1 - MAP
+#exemplo 4.1 - map()
 
 dobro = list(map(lambda x: x*2,lista))
 print(dobro)
+
 #exemplo 4.2 - FILTER
+
+valores = list(range(20,61))
+print(valores)
+
+pares = list(filter(lambda num:num % 2 == 0,valores))
+'''print("Os números pares são: \n",pares)''' #pode ser essa formatação ou com format :')
+print(f'Os números pares são: {pares}')
